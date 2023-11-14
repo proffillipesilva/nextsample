ea2ad8408420c3ec1c38be86a318c7b142a888f0
Fillipe de Souza Silva <fssilva@fiecalunos.lan>
2023-11-13 22:27:06 -0300

    Modificacao1

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
 
   return (
     <div>
-      <h2>My Amazing App</h2>
+      <h2>Enzo é um cara bem legal</h2>
       <>
       {sonoDoLucas.title && 
       <div id="toast-default" className="flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
